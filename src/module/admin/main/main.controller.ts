a345fd63293e468c4be6d153593b8e3ab3b8f19f
liang_dq@hisuntech.com <liang_dq@hisuntech.com>
2020-08-10 10:41:36 +0800

    docs: init feat: init

--- src/module/admin/main/main.controller.ts
+++ src/module/admin/main/main.controller.ts
@@ -3,7 +3,7 @@
  * @Author: Duchin/梁达钦
  * @Date: 2020-08-06 16:23:58
  * @LastEditors: Duchin/梁达钦
- * @LastEditTime: 2020-08-06 17:14:21
+ * @LastEditTime: 2020-08-06 18:48:43
  */
 import { Controller, Get, Render } from '@nestjs/common';
 
@@ -11,7 +11,17 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class MainController {
   @Get()
   @Render('admin/main/index')
-  index() {
+  index(): any {
     return {};
+  }
+
+  @Get('add')
+  add(): any {
+    return '我是管理员页面';
+  }
+
+  @Get('edit')
+  edit(): any {
+    return '编辑';
   }
 }
--- src/module/admin/manager/manager.controller.ts
+++ src/module/admin/manager/manager.controller.ts
@@ -3,14 +3,28 @@
  * @Author: Duchin/梁达钦
  * @Date: 2020-08-06 16:24:30
  * @LastEditors: Duchin/梁达钦
- * @LastEditTime: 2020-08-06 17:06:50
+ * @LastEditTime: 2020-08-09 12:34:57
  */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('admin/manager')
 export class ManagerController {
   @Get()
-  index() {
-    return '后台管理员';
+  @Render('admin/manager/index')
+  index(): any {
+    console.log('process.env.NODE_ENV;', process.env.NODE_ENV);
+    return process.env.NODE_ENV;
+  }
+
+  @Get('add')
+  @Render('admin/manager/add')
+  add(): any {
+    return {};
+  }
+
+  @Get('edit')
+  @Render('admin/manager/edit')
+  edit(): any {
+    return {};
   }
 }
